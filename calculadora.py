5e618ad5408461af16c40e9e1af354f55afddef9
AliceSoares0 <107281958+AliceSoares0@users.noreply.github.com>
2023-02-28 19:21:27 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+#função de somar dois valores
+def soma(num1, num2):
+     return num1 + num2
+#função de subritrair dois valores
+def sub(num1, num2):
+    return num1 - num2
+#função de multiplicar dois valores
+def multi(num1, num2) :
+    return num1 * num2
+#função de dividir dois valores
+def div(num1, num2):
+    return num1 / num2
